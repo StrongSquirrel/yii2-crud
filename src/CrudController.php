2742788743141ac3002149605c9474158162f500
frostealth <frostealth@gmail.com>
2015-12-13 15:55:36 +0600

    remove trait from CrudController

--- src/CrudController.php
+++ src/CrudController.php
@@ -11,8 +11,39 @@ use yii\web\Controller;
  */
 class CrudController extends Controller
 {
-    use CrudTrait;
-
     /** @var string */
     public $modelClass;
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'index' => [
+                'class' => IndexAction::className(),
+                'modelClass' => $this->modelClass,
+            ],
+            'view' => [
+                'class' => ViewAction::className(),
+                'modelClass' => $this->modelClass,
+            ],
+            'create' => [
+                'class' => CreateAction::className(),
+                'modelClass' => $this->modelClass,
+            ],
+            'update' => [
+                'class' => UpdateAction::className(),
+                'modelClass' => $this->modelClass,
+            ],
+            'delete' => [
+                'class' => DeleteAction::className(),
+                'modelClass' => $this->modelClass,
+            ],
+            'search' => [
+                'class' => SearchAction::className(),
+                'modelClass' => $this->modelClass,
+            ],
+        ];
+    }
 }
